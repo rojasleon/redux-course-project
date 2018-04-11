6f0a68981fe70886a511dd34d27e9b479c6d015e
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-11 13:45:42 -0500

    Updating the state

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+// Start todo
+{
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn redux',
+    complete: false
+  }
+}
+
+{
+  type: 'REMOVE_TODO',
+  id: 0
+}
+
+{
+  type: 'TOGGLE_TODO',
+  id: 0
+}
+// end todo
+
+// start goal
+{
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Learn GraphQl'
+  }
+}
+
+{
+  type: 'REMOVE_GOAL',
+  id: 0
+}
+// end goal
+
 function createStore() {
   // The store should have four parts
   // 1. The state
