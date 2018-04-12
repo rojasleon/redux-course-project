71c504d6fac70ca68da1c81dcf798a9bab824e17
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-11 22:07:18 -0500

    Handling more actions

--- index.js
+++ index.js
@@ -43,10 +43,18 @@
 
 // reducer
 function todos (state = [], action) {
-  if(action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo])
+    case 'REMOVE_TODO':
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO':
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete })
+      )
+    default:
+      return state
   }
-  return state
 }
 
 function createStore(reducer) {
@@ -83,6 +91,10 @@ function createStore(reducer) {
 }
 
 const store = createStore(todos)
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
 
 store.dispatch({
   type: 'ADD_TODO',
